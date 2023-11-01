c519fb71c9c45e5b0fb101471ab2b8f13cd41a54
reckyy <srreki98@gmail.com>
2023-11-01 11:03:15 +0900

    ⚡️ feat: sample_i18n実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,19 @@
 class BooksController < ApplicationController
   def index
-    @book = Book.all
+    @book_name = Book.find(21)
+    @book_cascade = Book.find(22)
+    @book_name = Book.find(23)
+    @book_name = Book.find(24)
+    @book_name = Book.find(25)
+    @book_name = Book.find(26)
+    @book_name = Book.find(27)
+    @book_name = Book.find(28)
+    @book_name = Book.find(29)
+    @book_name = Book.find(30)
+    @book_name = Book.find(31)
+    @book_name = Book.find(32)
+    @book_name = Book.find(33)
+    @book_name = Book.find(34)
+    @book_name = Book.find(35)
   end
 end
